0cea7445186287e72bdeba6b69f46f7a62298f44
sakhokaba <skhmomo.200@gmail.com>
2023-06-11 16:34:32 +0200

    ajout du dossier migration

---
+++ migrations/Version20230609121742.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230609121742 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE room ADD room_max VARCHAR(255) NOT NULL, ADD room_number INT NOT NULL, ADD room_haut_gamme VARCHAR(255) DEFAULT NULL, ADD room_confort VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE room DROP room_max, DROP room_number, DROP room_haut_gamme, DROP room_confort');
+    }
+}
